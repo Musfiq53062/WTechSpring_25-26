40279f0176cbc6de82f28e92ab32d0b187b94474
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-04-04 20:07:23 +0600

    updated

--- Self Practice/Code Writting/3/solution.js
+++ Self Practice/Code Writting/3/solution.js
@@ -4,20 +4,17 @@ const messageInput = document.getElementById("message");
 function validateAge() {
     let age = parseInt(ageInput.value);
 
-    // Handle empty input
     if (age=="") {
         messageInput.innerHTML = "";
         return;
     }
 
-    // Validation: negative age not allowed
     if (age < 0) {
         alert("Age cannot be negative. Resetting to 0.");
         age = 0;
         ageInput.value = 0;
     }
 
-    // Main logic (dynamic message)
     if (age < 40) {
         messageInput.innerHTML = "To be a part of the community, you need to at least 40";
         messageInput.style.color = "black";
