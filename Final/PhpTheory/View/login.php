e75033ca0e53473b0190413c76b933d6236c4d45
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-04-17 20:24:30 +0600

    variable declaration on php

--- Final/PhpTheory/View/login.php
+++ Final/PhpTheory/View/login.php
@@ -4,7 +4,24 @@
         <h2> My First PHP Page</h2>
         
         <?php
-        echo "This is PHP inside html";
+
+        $name = "Musfiq"; //string
+        $age = 23;        //integer
+        $ab = 34.5;       //float
+        $isAdmin= true;   //boolean
+        echo "Name: $name , Age: $age";
+        
+        /*Rules for Naming Variables: 
+            In PHP, all variables start with $
+            variable name -->
+                Must start with a letter or underscore _. 
+                Cannot start with a digit 
+                Can contain letters, digits, and underscores  
+                Are case-sensitive ($name ≠ $Name). 
+        */
+        
+                
+
         ?>
     </body>
 </html>
