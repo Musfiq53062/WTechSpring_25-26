39c05c423b628a44f3fb503da7a9c957e36908d0
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-04-20 06:55:31 +0600

    name validation

--- Final/Lab01/Controller/formValidation.php
+++ Final/Lab01/Controller/formValidation.php
@@ -1,3 +1,5 @@
+<?php
+
 $nameErr = "";
 $emailErr = "";
 $websiteErr = "";
@@ -24,4 +26,21 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     $comment = $_REQUEST["comment"];
     $gender = $_REQUEST["gender"];
 
+    if(!empty ($name) && strlen($name)>=3){
+        echo "Name: ".$name."<br>";
+    }
+    else{
+        if(empty($name)){
+            $nameErr = "Name is required";
+        }
+        else{
+            $nameErr = "Name must be at least 3 characters";
+        }
+    }
+
+
+    if(empty($email)){
+        $emailErr = "Email is required"
+    }
 }
+?>
