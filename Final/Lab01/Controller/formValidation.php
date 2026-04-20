4a3b52746ca0c2e44a4dba40a04a27c544ae66b0
Musfiq <rahmanmdmusfiqur3@gmail.com>
2026-04-20 06:42:56 +0600

    Initialize variables

--- Final/Lab01/Controller/formValidation.php
+++ Final/Lab01/Controller/formValidation.php
@@ -1,46 +1,11 @@
-<?php 
+$nameErr = "";
+$emailErr = "";
+$websiteErr = "";
+$genderErr = "";
+$commentErr = "";
 
-$name="";
-$password ="";
-$validpassword = "";
-
-if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
-        $name = $_POST["name"];
-        $email= $_POST["email"];
-        $website = $_POST["website"];
-
-        $name = $_REQUEST["name"];
-        $email= $_REQUEST["email"];
-        $website = $_REQUEST["website"];
-
-
-        if(!empty($name) && strlen($name)>=5)
-            {
-                echo "User Name: ".$name;
-            }
-            else{
-                echo "UserName must be greater than 5 char";
-            }
-
-
-            if(strlen($password)>4)
-                {
-                   $validpassword = $password;
-                   echo "Password: ".$validpassword;
-                }
-                else{
-                    
-                    $validpassword = "Password Must be 4 Digit minimum";
-                    
-                }
-
-
-    }
-
-
-
-
-
-
-?>
+$name = "";
+$email = "";
+$website = "";
+$comment = "";
+$gender = "";
