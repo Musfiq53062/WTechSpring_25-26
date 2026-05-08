e17e6231acc7f82d9ef2697660babc9e5ea175b7
Musfiq53062 <rahmanmdmusfiqur3@gmail.com>
2026-05-08 21:08:16 +0600

    array 3

--- Final/Array/problem1.php
+++ Final/Array/problem1.php
@@ -15,8 +15,20 @@ $prices =[250,645,300,900,50];
 for($i=0; $i<count($prices); $i++){
     $prices[$i]=$prices[$i]-$prices[$i]*0.01;
 }
-echo "Final prices after discount: ";
-print_r($prices);
+echo "<br>Final prices after discount: ";
+print_r($prices);echo "<br><br>";
 
+//3
+
+$companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+print_r($companies); echo "<br>";
+array_shift($companies);
+print_r($companies); echo "<br>";
+
+$uberIndex=array_search("Uber",$companies);
+$companies[$uberIndex]="Ola";
+print_r($companies); echo "<br>";
+array_push($companies,"Amazon");
+print_r($companies); echo "<br>";
 
 ?>
