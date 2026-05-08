3213dca2333b36ff8309e66f124d71678d10cec2
Musfiq53062 <rahmanmdmusfiqur3@gmail.com>
2026-05-08 20:07:45 +0600

    problem 2

--- Final/Array/problem1.php
+++ Final/Array/problem1.php
@@ -1,4 +1,6 @@
 <?php
+
+//1
 $marks =[85,94,38,58,95];
 
 $sum=0;
@@ -6,5 +8,15 @@ for($i=0; $i<count($marks); $i++){
     $sum+=$marks[$i];
 }
 $avg=$sum/count($marks);
-echo "Average mark: ".$avg;
+echo "Average mark: ".$avg."<br>";
+
+//2
+$prices =[250,645,300,900,50];
+for($i=0; $i<count($prices); $i++){
+    $prices[$i]=$prices[$i]-$prices[$i]*0.01;
+}
+echo "Final prices after discount: ";
+print_r($prices);
+
+
 ?>
